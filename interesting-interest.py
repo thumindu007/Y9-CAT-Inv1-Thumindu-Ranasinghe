fcf1d1dd07cd7aebfb933368dfaafa182991c5b2
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2024-03-02 06:52:34 +0000

    started module 2, made different function for getting compound interest

--- interesting-interest.py
+++ interesting-interest.py
@@ -1,12 +1,17 @@
 #started on 26/2
 
+def get_compounding_account():
+    Cstarting_amount, Cinterest_rate, Cinterest_rate_time, Compound_period = float(input("\nEnter the principal amount in $: ")), float(input("\nEnter the interest rate (enter 7%' as 7): ")), input("\nEnter the interest rate time unit (year, quarter, month, week, day): "), input("\nEnter the compounding period time unit (year, quarter, month, week, day, custom): ")
+    if Compound_period == 'custom':
+        Compound_period = float(input("\nEnter the number of compounding periods per interest rate time unit: "))
+    return {'starting_amount' : Cstarting_amount, 'interest_rate' : Cinterest_rate, 'interest_rate_time' : Cinterest_rate_time, 'Compound_period' : Compound_period}
+
 def get_data():
     print("Simple Interest Account:")
     Sstarting_amount, Sinterest_rate, Sinterest_rate_time,  = float(input("\nEnter the principal amount in $: ")), float(input("\nEnter the interest rate (enter 7%' as 7): ")), input("\nEnter the interest rate time unit (year, quarter, month, week, day): ")
     print("\n\nCompound Interest Account:")
-    Cstarting_amount, Cinterest_rate, Cinterest_rate_time, Compound_period = float(input("\nEnter the principal amount in $: ")), float(input("\nEnter the interest rate (enter 7%' as 7): ")), input("\nEnter the interest rate time unit (year, quarter, month, week, day): "), input("\nEnter the compounding period time unit (year, quarter, month, week, day, custom): ")
-    if Compound_period == 'custom':
-        Compound_period = float(input("\nEnter the number of compounding periods per interest rate time unit: "))
+    compound_account = get_compounding_account()
+    Cstarting_amount, Cinterest_rate, Cinterest_rate_time, Compound_period = compound_account[0], compound_account[1], compound_account[2], compound_account[3]
     print("\n\nFuture projection timeframe for both accounts:")
     time_into_future, unit_into_future = int(input("\nEnter the amount of time to project into the future: ")), input("\nEnter the projection time unit (year, quarter, month, week, day): ")
 
@@ -37,6 +42,7 @@ def interface():
         return False
     else:
         if module == 1:
+            print("\n***MODULE 1: SIMPLE AND COMPOUND INTEREST COMPARISON***")
             accounts = get_data()
             compounding_account, simple_account, time_into_future, unit_into_future = accounts[0], accounts[1], accounts[2], accounts[3]
 
@@ -46,6 +52,11 @@ def interface():
             print(f"\n\n\nSI Account projected amount: ${simple_account_calculated}, Interest earned: ${simple_account_calculated - simple_account['starting_amount']}")
             print(f"\nCI Account projected amount: ${compounding_account_calculated}, Interest earned: ${compounding_account_calculated - compounding_account['starting_amount']}")
         
+        if module == 2:
+            print("\n***MODULE 2: TIME FOR A CI ACCOUNT TO REACH A TARGET AMOUNT***")
+            compounding_account_target = get_compounding_account()
+            print(compounding_account_target)
+
         input("\n\nClick enter when you are done looking:  ")
         return True
 
